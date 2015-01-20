de6e47de66d067618a2f5e46d4f74c91dfc98162
robledop <robledo@gmail.com>
2015-01-20 00:14:04 -0200

    Initial Commit

---
+++ FormatAsCodeWLWPlugin/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Forms;
+using WindowsLive.Writer.Api;
+
+namespace FormatAsCodeWLWPlugin
+{
+    [WriterPlugin("EA4F0284-6C79-4D86-B556-A2029CB577D4", "Format as Code",PublisherUrl = "http://pazotto.com")]
+    [InsertableContentSource("Code")]
+    public class Plugin : ContentSource
+    {
+        public override DialogResult CreateContent(IWin32Window dialogOwner, ref string content)
+        {
+            string end = null;
+            if (content.EndsWith(" "))
+            {
+                end = " ";
+            }
+
+            string start = null;
+            if (content.StartsWith(" "))
+            {
+                start = " ";
+            }
+
+            if (!string.IsNullOrEmpty(content))
+                content = string.Format(start + "<code>{0}</code>" + end, content.Trim());
+            return DialogResult.OK;
+        }
+    }
+}
